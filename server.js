c246a1f733a7f53f1b3be1f8a75f9898f02bb97a
Oskar Lindblad <lindbladoskar@gmail.com>
2022-02-04 15:17:29 +0100

    heroku setup

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "index.html");
 });
 
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.on("send-message", (message) => {
+    socket.broadcast.emit("receive-message", message);
+  });
+});
+
 server.listen(PORT, () => {
   console.log(`Server started at ${PORT}`);
 });
