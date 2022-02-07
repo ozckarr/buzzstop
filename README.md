129a6d54440fd2d0c37aa70762cd2d6cc3202690
Oskar Lindblad <lindbladoskar@gmail.com>
2022-02-07 14:01:09 +0100

    Basic sound setup

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # buzzstop
-<<<<<<< HEAD
 
 At it's base a chat-app. But will become a browserbased buzzer-app for quizzes.
 
 npm run dev
-=======
-At it's base a chat-app. But will become a browserbased buzzer-app for quizzes.
->>>>>>> 0b32fe7d5a5382b9f3fa03abee05206642545afc
+
+credit to:
+Myles Ritter
+https://www.youtube.com/watch?v=6SdDT1xwUk4
+
+https://buzzstop.herokuapp.com/
---
+++ script.js
@@ -0,0 +1,34 @@
+const sendButton = document.getElementById("send_button");
+
+const socket = io.connect("https://buzzstop.herokuapp.com/");
+socket.on("connect", () => console.log("connected"));
+socket.on("receive-message", (message) => displayMessage(message));
+
+sendButton.addEventListener("click", () => {
+  const nameInput = document.getElementById("name_input");
+  const messageInput = document.getElementById("message_input");
+  const selectedSound = document.querySelector(
+    'input[name="picked_sound"]:checked'
+  ).value;
+  let message = {
+    name: nameInput.value === "" ? "Anonym" : nameInput.value,
+    selectedSound: selectedSound,
+    time: Date.now(),
+  };
+
+  if (messageInput === "") {
+    return;
+  }
+  displayMessage(message);
+  socket.emit("send-message", message);
+  messageInput.value = "";
+});
+
+const displayMessage = (message) => {
+  const messageContainer = document.getElementById("message_container");
+  const div = document.createElement("div");
+  div.id = "message_div";
+  new Audio(`./Audio/${message.selectedSound}.mp3`).play();
+  div.innerHTML = message.name;
+  messageContainer.append(div);
+};
