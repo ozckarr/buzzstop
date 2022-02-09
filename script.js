afb6b8e0f0805ccf39d38e7f5bfd682a0966255b
Oskar Lindblad <lindbladoskar@gmail.com>
2022-02-09 13:11:20 +0100

    clean up btn

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-window.onload = function () {
-  randomizeColor();
-};
+const socket = io.connect("https://buzzstop.herokuapp.com/");
 
 const sendButton = document.getElementById("send_button");
+const clearButton = document.getElementById("clear_button");
 const playerColor = document.getElementById("player_color");
 
-const socket = io.connect("https://buzzstop.herokuapp.com/");
-socket.on("connect", () => console.log("connected"));
-socket.on("receive-message", (message) => displayMessage(message));
+window.addEventListener("load", () => {
+  init();
+});
 
-sendButton.addEventListener("click", () => {
+const init = () => {
+  randomizeColor();
+  // Socket setup
+  socket.on("connect", () => console.log("connected"));
+  socket.on("receive-message", (message) => handleMessage(message));
+
+  sendButton.addEventListener("click", sendMsg);
+  clearButton.addEventListener("click", clearBtn);
+};
+
+const randomizeColor = () => {
+  let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  playerColor.value = randomColor;
+  document.getElementById("send_button").style.backgroundColor = randomColor;
+};
+
+// Send message
+const sendMsg = () => {
   const nameInput = document.getElementById("name_input");
   const selectedSound = document.querySelector(
     'input[name="picked_sound"]:checked'
@@ -23,8 +39,36 @@ sendButton.addEventListener("click", () => {
 
   displayMessage(message);
   socket.emit("send-message", message);
-});
+};
 
+// Clear
+const clearBtn = () => {
+  //Socket.io used wrong, but if it works it works...
+  let message = {
+    name: "clear",
+    selectedSound: "clear",
+    time: Date.now(),
+    playerColor: "#fff",
+  };
+  socket.emit("send-message", message);
+
+  clearMessages();
+};
+
+const clearMessages = () => {
+  const messageContainer = document.getElementById("message_container");
+  messageContainer.innerHTML = "";
+};
+
+const handleMessage = (message) => {
+  if (message.name === "clear") {
+    clearMessages();
+  } else {
+    displayMessage(message);
+  }
+};
+
+// Display buzzes
 const displayMessage = (message) => {
   const messageContainer = document.getElementById("message_container");
   const div = document.createElement("div");
@@ -36,13 +80,8 @@ const displayMessage = (message) => {
   messageContainer.append(div);
 };
 
+// Color
 playerColor.addEventListener("change", () => {
   const newColor = playerColor.value;
   document.getElementById("send_button").style.backgroundColor = newColor;
 });
-
-const randomizeColor = () => {
-  let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-  playerColor.value = randomColor;
-  document.getElementById("send_button").style.backgroundColor = randomColor;
-};
